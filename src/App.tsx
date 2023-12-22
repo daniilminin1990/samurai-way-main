519e885905ad10a664bad33228bfc00382bc968a
Daniil <daniilminin1990@yandex.ru>
2023-12-22 22:06:53 +0600

    15 компоненты в папках, модули прикручены

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from './Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <img
+        src="https://e7.pngegg.com/pngimages/92/187/png-clipart-logo-company-business-creative-company-logo-free-logo-design-template-company.png"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
