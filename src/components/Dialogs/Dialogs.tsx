381ba5756356a30aca746e2cb36b56e8472e7a79
Daniil <daniilminin1990@yandex.ru>
2024-01-06 19:50:42 +0600

    21 верстка страницы диалогов

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Oleg
+        </div>
+        <div className={s.dialog}>
+          Tanya
+        </div>
+        <div className={s.dialog}>
+          Victor
+        </div>
+        <div className={s.dialog}>
+          Elena
+        </div>
+        <div className={s.dialog}>
+          Valery
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi. How are you</div>
+        <div className={s.message}>Yo mazafaka</div>
+        <div className={s.message}>It is a great day innit?</div>
+      </div>
     </div>
   )
 }
