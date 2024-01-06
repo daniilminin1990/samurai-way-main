91437fc8dc499c43380116a054605815a143d780
Daniil <daniilminin1990@yandex.ru>
2024-01-06 20:57:50 +0600

    23 — props для Диалогов (инкапсуляция деталей в компонентах)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,30 +2,40 @@ import React from 'react'
 import s from './Dialogs.module.css'
 import { NavLink } from 'react-router-dom'
 
+const DialogItems = (props: { name: string, id: string }) => {
+  let path = '/dialogs/' + props.id
+  return (
+    <div className={s.dialog + ' ' + s.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div >
+  )
+}
+
+const Message = (props: { message: string }) => {
+  return (
+    <div className={s.message}>{props.message}</div>
+  )
+}
+
 const Dialogs = () => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog + ' ' + s.active}>
+        <DialogItems name='Ilya' id='1' />
+        <DialogItems name='Oleg' id='2' />
+        {/* <div className={s.dialog + ' ' + s.active}>
           <NavLink to='/dialogs/1'>Oleg</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to='/dialogs/2'>Tanya</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to='/dialogs/3'>Victor</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to='/dialogs/4'>Elena</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to='/dialogs/5'>Valery</NavLink>
-        </div>
+        </div> */}
+        <DialogItems name='Tanya' id='3' />
+        <DialogItems name='Victor' id='4' />
+        <DialogItems name='Elena' id='5' />
+        <DialogItems name='Valery' id='6' />
       </div>
       <div className={s.messages}>
-        <div className={s.message}>Hi. How are you</div>
-        <div className={s.message}>Yo mazafaka</div>
-        <div className={s.message}>It is a great day innit?</div>
+        <Message message='Hi. How are you' />
+        <Message message='Yo mazafaka' />
+        <Message message='It is a great day innit?' />
+        {/* <div className={s.message}>Hi. How are you</div> */}
       </div>
     </div>
   )
