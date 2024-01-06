0e4dc1f6d24e06f1cba56604084f5b3f3ce42ec6
Daniil <daniilminin1990@yandex.ru>
2024-01-06 19:03:38 +0600

    19 react-router-dom

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,19 +5,19 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a>Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a>Messages</a>
+        <a href="/dialogs">Messages</a>
       </div>
       <div className={s.item}>
-        <a>News</a>
+        <a >News</a>
       </div>
       <div className={s.item}>
-        <a>Music</a>
+        <a >Music</a>
       </div>
       <div className={s.item}>
-        <a>Settings</a>
+        <a >Settings</a>
       </div>
     </nav>
   );
